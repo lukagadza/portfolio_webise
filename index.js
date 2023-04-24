b626519d91ef5137ea6a9f135a66fa96b840eadb
Luka Gadza <129672573+lukagadza@users.noreply.github.com>
2023-04-24 19:21:13 +0200

    Animated text (frontend developer)

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+})
+
+
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
